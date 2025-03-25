4a73fdb4392d17997ee0779b82f58e4cfa1fdc5e
AlpinooO <alpino.cdc@gmail.com>
2025-03-25 15:46:52 +0100

    Update CI workflow to set executable permissions for gradlew and add unit test for Car class

--- src/test/java/com/example/DevSecOpsGRP2/DevSecOpsGrp2ApplicationTests.java
+++ src/test/java/com/example/DevSecOpsGRP2/DevSecOpsGrp2ApplicationTests.java
@@ -8,6 +8,9 @@ class DevSecOpsGrp2ApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Car car = new Car("AA11BB", "Ferrai", 2000);
+		String plateNumber = car.getPlateNumber();
+		assertEquals(plateNumber, "AA11BB");
 	}
 
 }
