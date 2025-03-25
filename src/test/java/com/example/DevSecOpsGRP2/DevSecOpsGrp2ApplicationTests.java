755f4061e7c40b6b9a744f9b2d59b0c4110e3990
AlpinooO <alpino.cdc@gmail.com>
2025-03-25 18:40:59 +0100

    Refactor application class name and update unit tests; add Dockerfile for containerization

--- src/test/java/com/example/DevSecOpsGRP2/DevSecOpsGrp2ApplicationTests.java
+++ src/test/java/com/example/DevSecOpsGRP2/DevSecOpsGrp2ApplicationTests.java
@@ -2,15 +2,15 @@ package com.example.DevSecOpsGRP2;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-
 @SpringBootTest
-class DevSecOpsGrp2ApplicationTests {
+class DevSecOpsGRP2ApplicationTests {
 
 	@Test
 	void contextLoads() {
-		Car car = new Car("AA11BB", "Ferrai", 2000);
+		Car car = new Car("AA11BB", "ferrari", 2000);
 		String plateNumber = car.getPlateNumber();
 		assertEquals(plateNumber, "AA11BB");
 	}
