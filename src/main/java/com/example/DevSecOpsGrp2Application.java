9d5d09dcbfd5c0aa38b5fe8fc7010a8ba366f230
AlpinooO <alpino.cdc@gmail.com>
2025-03-26 15:04:46 +0100

    Correction 1

--- src/main/java/com/example/DevSecOpsGrp2Application.java
+++ src/main/java/com/example/DevSecOpsGrp2Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DevSecOpsGRP2Application {
+public class DevSecOpsGrp2Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DevSecOpsGRP2Application.class, args);
+		SpringApplication.run(Devsecops2Application.class, args);
 	}
 
 }
--- src/test/java/com/example/DevSecOpsGRP2/DevSecOpsGrp2ApplicationTests.java
+++ src/test/java/com/example/DevSecOpsGRP2/DevSecOpsGrp2ApplicationTests.java
@@ -6,11 +6,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
-class DevSecOpsGRP2ApplicationTests {
+class Devsecops2ApplicationTests {
 
 	@Test
 	void contextLoads() {
-		Car car = new Car("AA11BB", "ferrari", 2000);
+		Car car = new Car("AA11BB", "Ferrai", 2000);
 		String plateNumber = car.getPlateNumber();
 		assertEquals(plateNumber, "AA11BB");
 	}
